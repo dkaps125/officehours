6ace02b219debcb0bb235725ce0385db22501889
Greg Bekher <greg@bekher.me>
2018-01-24 17:43:43 -0500

    add first and last name options to script

--- scripts/user.js
+++ scripts/user.js
@@ -14,12 +14,13 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.mongodb);
 
 program
-  .command('adduser <username> <role>')
+  .command('adduser <username> <role> <firstname> <lastname>')
   .description('add a user')
-  .action(function(directoryID, role, options) {
+  .action(function(directoryID, role, first, last, options) {
     app.service('users').create({
       directoryID: directoryID,
       role: role,
+      name: first+" "+last,
     }).then(user => {
       console.log('created user '+user);
       process.exit(0);
