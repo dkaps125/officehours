1e06297f4b233f2ab95e101be0f6536f7bfd7581
dkaps125 <dkapit@umd.edu>
2018-01-26 20:09:36 -0500

    Added searchability to users table

--- public/js/instructor.js
+++ public/js/instructor.js
@@ -250,3 +250,22 @@ $(function() {
     });
   });
 });
+
+function search() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchBox");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("userTable");
+  tr = table.getElementsByTagName("tr");
+   
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  } 
+}
