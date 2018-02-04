7730cd2cd08586ad6318e35410cba32012ca5ee4
Greg Bekher <greg@bekher.me>
2018-02-04 12:36:45 -0500

    I forgot we're using sass not less

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
+var rename = require('gulp-rename');
+const del = require('del');
 
 gulp.task('css', function() {
-  return gulp.src('client/less/*.less')
-    .pipe(less())
+  return gulp.src('./client/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('public/css'))
+    .pipe(rename({
+      suffix: '.min'
+     }))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('sass:watch', function() {
+   gulp.watch('./client/scss/*.scss', ['sass']);
+ });
+
+gulp.task('clean', function(cb) {
+  del(['./public/*'], cb);
 });
 
 gulp.task('default', ['css']);
