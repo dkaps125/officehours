11485ccb6646011c3e5ccaf9c7c2012f085bf0cb
Greg Bekher <greg@bekher.me>
2018-02-26 20:56:06 -0500

    add script to associate total tickets with each user

--- scripts/assocUser.js
+++ scripts/assocUser.js
@@ -13,15 +13,15 @@ mongoose.Promise = global.Promise;
 
 //mongoose.connect(config.mongodb);
 
-// This must be exec'd before the search service can work: 
+// This must be exec'd before the search service can work:
 // db.tokens.dropIndex("desc_text_user_text_fulfilledByName_text");
 program
   .command('addUserName')
-  .description('update columns')
+  .description('update columns for token fulfilledByName')
   .action(function() {
     app.service('tokens').find({
       query: {
-        $limit: 1000,
+        $limit: 1500,
       }
     }).then(tickets => {
       tickets.data.map(ticket => {
@@ -46,5 +46,48 @@ program
         process.exit(1);
     });
   });
+
+  program
+    .command('addUserTotal')
+    .description('update columns for user total')
+    .action(function() {
+      app.service('/users').find({
+      }).then(users => {
+        users.data.map(user => {
+          var q = {
+            query: {
+              $limit: 0,
+              user: user._id
+            }
+          }
+          if (user.role !== "Student") {
+            q.query = {
+              $limit: 0,
+              fulfilledBy: user._id
+            }
+          }
+          app.service('/tokens').find(
+            q
+          ).then(tokens => {
+            console.log(user.name + ': ' + tokens.total);
+            return app.service('/users').patch(user._id, {
+              totalTickets: tokens.total
+            });
+          }).catch(err => {
+            console.error('mongoose error 2');
+            console.error(err);
+            process.exit(1);
+          });
+        });
+        // map is synchronous apparently
+        //process.exit(0);
+
+      }).catch(function(err) {
+          console.error('mongoose error');
+          console.error(err);
+          process.exit(1);
+      });
+    });
+
 
 program.parse(process.argv);
