2e2d770f7bf7b018f7a6e723488b250bff82e087
dkaps125 <dkapit@umd.edu>
2018-08-26 14:16:34 -0400

    Added course param to csv upload

--- src/middleware/csv.js
+++ src/middleware/csv.js
@@ -5,78 +5,114 @@ const stringify = require('csv-stringify');
 const csv = require('csv-parse');
 const fs = require('fs');
 
-module.exports = function (options = {}) {
-  const app = options.app
+module.exports = function(options = {}) {
+  const app = options.app;
   const userQuery = app.service('users');
+  const courseQuery = app.service('courses');
 
   return function userCSV(req, res, next) {
     var failed = false;
-    var failure = (err) => {
+    var failure = err => {
       if (!failed) {
         failed = true;
         res.status(400);
         res.format({
           'application/json': () => {
-            res.json({error: {"cause": err, "isOperational": true}, status: "error"});
+            res.json({ error: { cause: err, isOperational: true }, status: 'error' });
           }
         });
         console.log(err);
       }
+    };
+
+    if (req.user.role !== 'Instructor') {
+      failure('unauthorized');
     }
 
-    if (req.user.role !== "Instructor") {
-      failure("unauthorized");
-    }
-
-    const parseUser = transform( function(record, cb) {
+    const parseUser = transform(function(record, cb) {
       if (!record || !record.name || !record.directoryId) {
-        failure("Malformed CSV record, missing name or directoryID");
+        failure('Malformed CSV record, missing name or directoryID');
         return;
       }
-      if (!!record.role && record.role.length > 0 && record.role.toLowerCase() != "ta") {
-        record.role = record.role.substr(0,1).toUpperCase() + record.role.substr(1,record.length).toLowerCase();
-      } else if (!!record.role && record.role.toUpperCase() == "TA") {
+      if (!!record.role && record.role.length > 0 && record.role.toLowerCase() != 'ta') {
+        record.role = record.role.substr(0, 1).toUpperCase() + record.role.substr(1, record.length).toLowerCase();
+      } else if (!!record.role && record.role.toUpperCase() == 'TA') {
         record.role = record.role.toUpperCase();
       }
-      if (record.role !== "Student" && record.role !== "Instructor" && record.role !== "TA") {
-        record.role = "Student";
+      if (record.role !== 'Student' && record.role !== 'Instructor' && record.role !== 'TA') {
+        record.role = 'Student';
       }
 
-      userQuery.create({
+      var query = {
         directoryID: xss(record.directoryId),
         name: xss(record.name),
         role: xss(record.role)
-      }).then(res => {
-        cb();
-      }).catch(err => {
-        cb();
-      });
+      };
+
+      if (!!record.course) {
+        courseQuery
+          .find({
+            query: {
+              courseid: record.course
+            }
+          })
+          .then(res => {
+            query.roles = [
+              {
+                course: res.data[0]['_id'],
+                privilege: query.role
+              }
+            ];
+
+            cb();
+            userQuery
+              .create(query)
+              .then(res => {
+                cb();
+              })
+              .catch(err => {
+                cb();
+              });
+          })
+          .catch(err => {
+            cb();
+          });
+      } else {
+        userQuery
+          .create(query)
+          .then(res => {
+            cb();
+          })
+          .catch(err => {
+            cb();
+          });
+      }
     });
 
-    var parser = csv({columns: true})
-    .on('error', err => {
-      failure(err.message || "Malformed CSV");
+    var parser = csv({ columns: true }).on('error', err => {
+      failure(err.message || 'Malformed CSV');
     });
     const csvInput = fs.createReadStream(req.file.path);
-    csvInput.pipe(parser)
-    .pipe(parseUser)
-    .on('finish', () => {
-      if (!failed) {
-        res.status(200);
-        res.format({
-          'application/json': () => {
-            res.json({status: "success"});
-          }
-        });
-      }
-      if (!!req.file && !!req.file.path) {
-        fs.unlink(req.file.path, (err) => {
-          if (err) failure(err.message || "Could not unlink file");
-        });
-      }
-    })
-    .on('error', err => {
-      failure(err.message || "Malformed CSV passed");
-    });
+    csvInput
+      .pipe(parser)
+      .pipe(parseUser)
+      .on('finish', () => {
+        if (!failed) {
+          res.status(200);
+          res.format({
+            'application/json': () => {
+              res.json({ status: 'success' });
+            }
+          });
+        }
+        if (!!req.file && !!req.file.path) {
+          fs.unlink(req.file.path, err => {
+            if (err) failure(err.message || 'Could not unlink file');
+          });
+        }
+      })
+      .on('error', err => {
+        failure(err.message || 'Malformed CSV passed');
+      });
   };
 };
